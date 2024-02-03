8790a26169be3cc9c244282d6208b3ce4013823e
qkunZhang <154985190+qkunZhang@users.noreply.github.com>
2024-02-03 22:04:04 +0800

    Delete back directory

--- back/src/main/java/com/back/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.back.service;
-
-import com.back.entity.UserEntity;
-import com.baomidou.mybatisplus.extension.service.IService;
-
-public interface UserService extends IService<UserEntity> {
-
-    public UserEntity getUserById(Integer id);
-}
